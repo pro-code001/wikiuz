85da22d402c4691b8711dc44d2624901fc503726
pro-code001 <fragmenfragmen777@gmail.com>
2025-06-06 07:39:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, abort
+import os
 
 app = Flask(__name__)
 
@@ -6,12 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/persons/<person_name>')
-def person_page(person_name):
-    try:
-        return render_template(f'persons/{person_name}.html')
-    except:
+@app.route('/persons/<name>')
+def person(name):
+    filename = f'persons/{name}.html'
+    path = os.path.join('templates', filename)
+    if os.path.exists(path):
+        return render_template(filename)
+    else:
         abort(404)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
