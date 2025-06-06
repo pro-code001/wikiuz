a6be116e0baa6dd02f18bddec3559f28b5f39cad
pro-code001 <fragmenfragmen777@gmail.com>
2025-06-06 07:27:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/persons/<person_name>')
-def show_person(person_name):
-    try:
-        return render_template(f'persons/{person_name}.html')
-    except:
-        abort(404)  # Agar fayl topilmasa, 404 xatolik ko'rsatish
+@app.route('/persons/alishernavoiy')
+def alishernavoiy():
+    return render_template('persons/alishernavoiy.html')
